9eb501cb78f49cec0b31d3e249f9e517300e5aaf
mwangaza12 <josephmwangaza1@gmail.com>
2025-06-02 18:58:20 +0300

    refactored code

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,6 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { userRouter } from './routes/users.route';
 
-
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 3000;
@@ -10,8 +9,7 @@ const PORT = process.env.PORT || 3000;
 // Default route
 app.get('/', (req, res) => {
   res.send('Welcome to Expressjs + drizzle ORM with MVC architecture!');
-}
-);
+});
 
 // default middleware
 app.use(express.json());
@@ -20,8 +18,6 @@ app.use(express.urlencoded({ extended: true }));
 // Importing routes
 app.use('/api', userRouter);
 
-
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
-}
-);
+});
